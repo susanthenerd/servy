44b82af4c2e86e4e2e61c5d091ff9308763ff3a0
Emily Susan <susan@susan.lol>
2023-12-29 13:01:03 +0200

    Now it has GenServer

--- lib/servy/four_oh_four_counter.ex
+++ lib/servy/four_oh_four_counter.ex
@@ -1,48 +1,38 @@
 defmodule Servy.FourOhFourCounter do
   @name :four_oh_four_counter
-
-  alias Servy.GenericServer
+  use GenServer
 
   # Client Interface
 
-  def start do
-    IO.puts("Starting the 404 counter...")
-    GenericServer.start(__MODULE__, %{}, @name)
-  end
+  def start, do: GenServer.start(__MODULE__, %{}, name: @name)
 
-  def bump_count(path) do
-    GenericServer.call(@name, {:bump_count, path})
-  end
+  def init(state), do: state
 
-  def get_counts do
-    GenericServer.call(@name, :get_counts)
-  end
+  def bump_count(path), do: GenServer.call(@name, {:bump_count, path})
 
-  def get_count(path) do
-    GenericServer.call(@name, {:get_count, path})
-  end
+  def get_counts, do: GenServer.call(@name, :get_counts)
 
-  def reset do
-    GenericServer.cast(@name, :reset)
-  end
+  def get_count(path), do: GenServer.call(@name, {:get_count, path})
+
+  def reset, do: GenServer.cast(@name, :reset)
 
   # Server Callbacks
 
-  def handle_call({:bump_count, path}, state) do
+  def handle_call({:bump_count, path}, _from, state) do
     new_state = Map.update(state, path, 1, &(&1 + 1))
-    {:ok, new_state}
+    {:reply, :ok, new_state}
   end
 
   def handle_call(:get_counts, state) do
-    {state, state}
+    {:reply, state, state}
   end
 
   def handle_call({:get_count, path}, state) do
     count = Map.get(state, path, 0)
-    {count, state}
+    {:reply, count, state}
   end
 
   def handle_cast(:reset, _state) do
-    %{}
+    {:noreply, %{}}
   end
 end
--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,47 +1,71 @@
 defmodule Servy.PledgeServer do
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
+
   require Logger
-  alias Servy.GenericServer
+  use GenServer
 
   @name :pledge_server
 
   def start do
-    GenericServer.start(__MODULE__, [], @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
+  end
+
+  def init(%State{} = args) do
+    pledges = fetch_recent_pledges_from_service()
+
+    state = %{args | pledges: pledges}
+
+    {:ok, state}
   end
 
   # Client
 
-  def create_pledge(name, amount), do: GenericServer.call(@name, {:create_pledge, name, amount})
-  def recent_pledges, do: GenericServer.call(@name, :recent_pledges)
-  def total_pledged, do: GenericServer.call(@name, :total_pledged)
+  def create_pledge(name, amount), do: GenServer.call(@name, {:create_pledge, name, amount})
+  def recent_pledges, do: GenServer.call(@name, :recent_pledges)
+  def total_pledged, do: GenServer.call(@name, :total_pledged)
 
-  def clear, do: GenericServer.cast(@name, :clear)
+  def set_cache_size(size), do: GenServer.cast(@name, {:set_cache_size, size})
+  def clear, do: GenServer.cast(@name, :clear)
 
   # Server Callback
 
-  def handle_call(:total_pledged, state) do
-    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum()
-    {total, state}
+  def handle_call(:total_pledged, _from, state) do
+    total = Enum.map(state.pledges, &elem(&1, 1)) |> Enum.sum()
+    {:reply, total, state}
   end
 
-  def handle_call(:recent_pledges, state) do
-    {state, state}
+  def handle_call(:recent_pledges, _from, state) do
+    {:reply, state.pledges, state}
   end
 
-  def handle_call({:create_pledge, name, ammount}, state) do
+  def handle_call({:create_pledge, name, ammount}, _from, state) do
     {:ok, id} = send_pledge_to_service(name, ammount)
 
-    most_recent_pleges = Enum.take(state, 2)
-    new_state = [{name, ammount} | most_recent_pleges]
+    most_recent_pledges = Enum.take(state.pledges, state.cache_size - 1)
+    cached_pledges = [{name, ammount} | most_recent_pledges]
 
-    {id, new_state}
+    new_state = %{state | pledges: cached_pledges}
+
+    {:reply, id, new_state}
   end
 
-  def handle_cast(:clear, _state) do
-    []
+  def handle_cast({:set_cache_size, size}, state) do
+    {:noreply, %{state | cache_size: size}}
+  end
+
+  def handle_cast(:clear, state) do
+    {:noreply, %{state | pledges: []}}
   end
 
   defp send_pledge_to_service(_name, _amount) do
     # fake send to service module
     {:ok, "pledge-#{:rand.uniform(1000)}"}
+  end
+
+  defp fetch_recent_pledges_from_service do
+    # Example return value:
+    [{"wilma", 15}, {"fred", 25}]
   end
 end
